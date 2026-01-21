e98800956a34470d0fd506d05de8e0dc8e749b69
Jesus-Barraza <190627332+Jesus-Barraza@users.noreply.github.com>
2026-01-21 08:48:01 -0600

    Update App.js

--- barraza_parcial_1/src/App.js
+++ barraza_parcial_1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola soy Jesús Daniel Barraza Torres, Este es mi hola mundo de react
         </p>
         <a
           className="App-link"
